ff52505ad3bb4c5a9c4d0ae77041d4fde40168dc
= <->
2018-08-08 22:59:29 +0700

    [ refactor part 1 ]

--- source/calculation.py
+++ source/calculation.py
@@ -1,141 +1,117 @@
 import threading
 from time import time as currentTime
 
-import config
+from config import FREQ_TO_VELOCITY, PLAYER_LIMIT
+from data import GameData, PLAYERSTATE, GAMESTATE
 
-
-class GameState():
-    READY = 0
-    PLAY = 1
-    END = 2
-
-class PlayerState():
-    STOP = -1
-    READY = 0
-    RIDE = 1
-    FINISH = 2
-
+#todo : game state change READY->PLAY by time trigger. not by player position
 
 class GameCalculationThread(threading.Thread):
     
-    ############
-    ## STATIC ##
-    ############
-    GAMESTATE = GameState()
-    PLAYERSTATE = PlayerState()
-    
-    def __init__(self, playerList = {}):
+    def __init__(self):
         super().__init__(name="GameCalculationThread")
-        self.playerList = playerList
-        self.previousTimeStamp = None
-        self.previousFreq = 0 #use for velocity calculation
+        self.gameData = GameData()
 
     ##############
     ## OVERRIDE ##
     ##############
     def run(self):
         print('start main game calculation thread')
-        self.__initialize()
         
         while True:
             self.deltaTime = self.__calculateDeltaTime()
             self.__updateGame(self.deltaTime)
             self.__updateTimeStamp()
 
+
     #####################
     ## PRIVATE HELPERS ##
     #####################
-    def __initialize(self):
-        self.previousTimeStamp = currentTime()
-        self.gameState = self.GAMESTATE.READY
-        self.playerPositionDict = {}
-        self.playerVelocityDict = {}
-
     def __calculateDeltaTime(self):
         return currentTime() - self.previousTimeStamp
     
     def __updateGame(self, deltaTime):
-        #if((len(self.playerList)>= 1)and(self.gameState is not self.GAMESTATE.END)):
-            #self.__setplayerVelocityDict()
-            #self.__setplayerPositionDict()
-            #self.__setGameStateByplay()
+        for player in self.gameData.players:
+            GameCalculator.updatePlayerPosition( player, deltaTime )
+            GameCalculator.updatePlayerState( player )
 
-        for player in self.playerList:
-            self.__velocityCalculation(player.getGameData())
-            self.__positionCalculation(player.getGameData())
-            self.__changePlayerState(player.getGameData())
-            
-            
+        GameCalculator.updateGameState( self.gameData
 
     def __updateTimeStamp(self):
         self.previousTimeStamp = currentTime()
-
-    #####################
-    ## PRIVATE SETTER  ##
-    #####################
-    def __setplayerPositionDict(self):
-        for player in self.playerList:
-            self.playerPositionDict[player] = player.getPosition()
             
-    def __setplayerVelocityDict(self):
-        for player in self.playerList:
-            self.playerPositionDict[player] = player.getVelocity()
-            
-    def __setGameStateByplay(self):
-        for player,position in self.playerPositionDict.items():
-            if(player.getPlayerState() == 2):
-                self.gameState = self.GAMESTATE.END
-            else:
-                self.gameState = self.GAMESTATE.PLAY
-                break
-            
-    ##################
-    ## PUBIC SETTER ##
-    ##################
-    def setGameState(self, gamestate): #Admin
-        self.gameState = gamestate
-
-    def setPlayerList(self,playerList):
-        self.playerList = playerList
+    ###################
+    ## PUBIC MUTATOR ##
+    ###################
+    def resetGame(self):
+        self.gameData.reset()
+        self.previousTimeStamp = currentTime()
 
     ##################
     ## PUBIC GETTER ##
     ##################
-    def getPlayerPositionDict(self):
-        return self.playerPositionDict
+    def getPlayerData(self, i):
+        return self.gameData.players[i]
     
     def getGameState(self):
-        return self.gameState
+        return self.gameData.gameState
    
     #####################
     ## PRIVATE HELPPER ##
     #####################
-    def __positionCalculation(self,player):
-        positionchange = player.getVelocity() * self.deltaTime
-        if positionchange > config.POSITION_LIMIT:
-            pos = player.getPosition()+config.POSITION_LIMIT
-            player.setPosition(pos)
-        else:
-            pos = player.getPosition()+ positionchange
-            player.setPosition(pos)
-
-    def __changePlayerState(self,player):
-        if (player.getVelocity() < 0):
-            player.setPlayerStateByPlay(self.PLAYERSTATE.STOP)
-        elif(player.getVelocity() == 0):
-            player.setPlayerStateByPlay(self.PLAYERSTATE.READY)
-        elif(player.getVelocity() > 0):
-            player.setPlayerStateByPlay(self.PLAYERSTATE.RIDE)
-        elif(player.getPosition() > 1):
-            player.setPlayerStateByPlay(self.PLAYERSTATE.FINISH)
-
-    
-    def __velocityCalculation(self,player):
-        currentFreq = player.getFrequency()
-        if(currentFreq != 0):
-            averageFreq = (currentFreq + self.previousFreq)/2
-            self.previousFreq =  currentFreq
-            velocity = (config.ENDING_POSITION /(config.AVERAGE_TIME*averageFreq))*currentFreq
-            player.setVelocity(velocity)
+    # -- nothing now -- #
+    #####################
         
-        1
+
+class GameCalculator:
+    @staticmethod
+    def updatePlayerPosition(player, deltaTime):
+        currentFrequency = player.getFrequency()
+        currentPosition = player.getPosition()
+        
+        if currentFreq is not 0:
+            updatedVelocity = FREQ_TO_VELOCITY * currentFrequency
+            updatedPosition = currentPosition + updatedVelocity * deltaTime
+            player.setVelocity( updatedVelocity )
+            player.setPosition( updatedPosition )
+        else:
+            player.setVelocity( 0 )
+            
+    @staticmethod
+    def updatePlayerState( self, player ):
+        playerPosition = player.getPosition()
+        
+        if playerPosition <= 0:
+            updatedState = PLAYERSTATE.READY
+        elif playerPosition >= 1:
+            updatedState = PLAYERSTATE.FINISHED
+        else: # in between (0, 1)
+            updatedState = PLAYERSTATE.RIDING
+
+        player.setPlayerState( updatedPlayerState )
+
+    @staticmethod
+    def updateGameState( gameData ):
+        allReady = True
+        allFinished = True
+        someFinished = False
+        
+        for player in gameData.players:
+            state = player.getPlayerState()
+            if state is not PLAYERSTATE.READY:
+                allReady = False
+            if state is not PLAYERSTATE.FINISHED:
+                allFinished = False
+            if state is PLAYER.FINISHED:
+                someFinished = True
+
+        if allReady:
+            updatedState = GAMESTATE.READY
+        elif allFinished:
+            updatedState = GAMESTATE.ALL_FINISHED
+        elif someFinished:
+            updatedState = GAMESTATE.FIRST_FINISHED
+        else:
+            updatedState = GAMESTATE.PLAYING_NO_WINNER
+
+        gameData.gameState = updatedState
