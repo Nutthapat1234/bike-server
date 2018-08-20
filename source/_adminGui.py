cc73ada8b39f70647a4e63eb38ca7a42adc3b0cd
Nutthapat Phoomara <Nutp1999@gmail.com>
2018-08-20 15:06:34 +0700

    admin v.2

--- source/_adminGui.py
+++ source/_adminGui.py
@@ -6,10 +6,11 @@ from _admin import Admin
 
 if __name__ == "__main__":
     windows = Tk()
+    windows.title('Admin')
     admin = Admin()
     hostname = socket.gethostbyname('localhost')
-    admin.connectToSever(hostname,1995)
-    startButt = Button(windows,text="Start",command = admin.start).pack()
-    resetButt = Button(windows,text="Reset",command = admin.reset).pack()
+    #admin.connectToSever(hostname,1995)
+    startButt = Button(windows,text="Start",command = admin.start,height = 5,width=30).pack(fill=BOTH)
+    resetButt = Button(windows,text="Reset",command = admin.reset,height = 5,width=30).pack(fill=BOTH)
     
     windows.mainloop()
